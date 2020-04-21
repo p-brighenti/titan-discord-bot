e4314c5e23a35eca105213ba310de41ebfe8cffb
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-21 22:23:12 +0100

    added ec2 config

---
+++ src/config/forever.js
@@ -0,0 +1,11 @@
+{
+    // Comments are supported
+    "uid": "titan-bot",
+    "append": true,
+    "watch": true,
+    "script": "src/index.js",
+    "sourceDir": "/home/ec2-user/titan-discord-bot",
+    "logFile": "/home/myuser/logs/forever.log",
+    "outFile": "/home/myuser/logs/out.log",
+    "errFile": "/home/myuser/logs/error.log"
+}
