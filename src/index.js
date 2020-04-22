659135059310242acb7cc7d4c41e6c4d344d6fb3
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-22 10:13:25 +0100

    added comment to date-calculator and moved dotenv to the top of requires

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const Discord = require('discord.js');
 const tcDecksService = require('./services/tcdecks');
 const goldFishService = require('./services/goldfish');
 const embedder = require('./utils/embedder');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const client = new Discord.Client();
 const prefix = process.env.BOT_PREFIX;
 
 client.on('ready', async () => {
-    console.log('Connected');
+    console.log(`Connected at ${new Date()}`);
 });
 
 client.on('message', async (message) => {
--- src/utils/date-calculator.js
+++ src/utils/date-calculator.js
@@ -1,4 +1,5 @@
 const dayInMilliseconds = 86400000;
 
+// (most likely) does not account for daylight savings
 exports.weeksPriorTo = (dateMilis, weeks = 1) =>
     new Date(dateMilis - dayInMilliseconds * weeks * 7);
