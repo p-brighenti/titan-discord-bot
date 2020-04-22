6813d168f417471c70c00da6fdfc04fddef69632
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-22 23:16:38 +0100

    added store with read method

---
+++ src/config/file-storage.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const config = {
+    basePath: path.normalize(`${__dirname}/../../store`),
+    encoding: 'utf-8',
+};
+
+Object.freeze(config);
+
+module.exports = config;
