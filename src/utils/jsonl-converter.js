49180e3116c48fc7dba3804573dcf20747ed275b
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-23 20:37:36 +0100

    refactor, added append method to file-storage

--- src/utils/jsonl-converter.js
+++ src/utils/jsonl-converter.js
@@ -1,13 +1,26 @@
+// TODO: needs to be able to handle functions
+
 exports.toJSON = (jsonl) =>
     `[${jsonl.replace(/(\n|\r|\r\n)/g, ',').slice(0, -1)}]`;
 
 // TODO: create new solution. will not work for entries storing an array of objects
-exports.stringify = (json) => {
-    if (typeof json == 'object') {
-        return exports.toJSONL(exports.toJSON(json));
+exports.stringify = (data) => {
+    const validShape = isValidShape(data);
+
+    if (!validShape && typeof data !== 'string') {
+        throw new Error('invalid JSONL object');
     }
 
-    return `${json.substring(1, json.length - 1).replace(/},{/g, '}\n{')}\n`;
+    if (validShape) {
+        console.log(data);
+        data = JSON.stringify(data);
+    }
+
+    return `${data.substring(1, data.length - 1).replace(/},{/g, '}\n{')}\n`;
 };
 
 exports.parse = (jsonl) => JSON.parse(exports.toJSON(jsonl));
+
+function isValidShape(obj) {
+    return Array.isArray(obj) && obj.every((elem) => typeof elem === 'object');
+}
