0382b9c69bb81b27af96ea084d5f85518ee38f43
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-24 14:41:46 +0100

    reverted changes

--- src/enums/actions.js
+++ src/enums/actions.js
@@ -1,10 +1,13 @@
 const embedder = require('../utils/embedder');
 
 const actions = {
-    POST_LISTS: (channel, post) => {
-        if (!post.data.length) return;
+    POST_LISTS: (channel, ...posts) => {
+        posts.forEach((post) => {
+            console.log('POST', post);
+            if (!post.data.length) return;
 
-        channel.send(embedder.build(post));
+            channel.send(embedder.build(post));
+        });
     },
 };
 
