f1f835111816b98702cc91cdf900c742fdef5242
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-24 14:43:52 +0100

    removed extraneous log

--- src/enums/actions.js
+++ src/enums/actions.js
@@ -3,7 +3,6 @@ const embedder = require('../utils/embedder');
 const actions = {
     POST_LISTS: (channel, ...posts) => {
         posts.forEach((post) => {
-            console.log('POST', post);
             if (!post.data.length) return;
 
             channel.send(embedder.build(post));
